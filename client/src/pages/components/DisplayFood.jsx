083d1ab2ddea2f23c51e5c5dd91a1fd4c686d09a
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2024-07-02 00:29:09 +0530

    third commit

--- client/src/pages/components/DisplayFood.jsx
+++ client/src/pages/components/DisplayFood.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 const Displayrecipe = () => {
   return (
     <>
-    <div className='h-auto w-auto flex flex-col'>
+    <div className='h-auto w-60 flex flex-col'>
       <span className='w-60 h-60 border rounded-lg'>
 
       </span>
-      
+
+      <span className='flex justify-around items-center w-full h-8 mt-2'>
+        <button className='w-auto h-7 rounded-lg bg-slate-500 pr-1 pl-1'>optiona</button>
+        <button className='w-auto h-7 rounded-lg bg-slate-500 pr-1 pl-1'>optionb</button>
+        <button className='w-auto h-7 rounded-lg bg-slate-500 pr-1 pl-1'>optionc</button>
+      </span>
     </div>
     </>
   )
