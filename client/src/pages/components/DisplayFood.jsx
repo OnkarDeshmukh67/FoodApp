4bd0d1c806daf98a0d1d10fd0c4ab8a614145f1c
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2024-07-03 18:56:10 +0530

    seventh commit

--- client/src/pages/components/DisplayFood.jsx
+++ client/src/pages/components/DisplayFood.jsx
@@ -2,12 +2,20 @@ import React, { useState } from 'react'
 
 const Displayrecipe = () => {
 
+  const reps = [0 , 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10]
+
   return (
     <>
     <div className='h-auto w-60 flex flex-col chakra-petch-light'>
-      {/* <p className='w-60 h-8 text-center text-xl italic'>Guess this dish...</p> */}
+      <p className='w-60 h-8 text-center text-xl'>Find Delicacies NearBy...</p>
 
-      <span className='w-60 h-40'></span> 
+      <span className='w-60 h-60 overflow-hidden rounded-md flex flex-wrap backdrop-blur-xl'>
+        
+        {reps.map(()=>{
+          return <img className='w-16 h-16 rounded-md m-2' src="/healthy_food/food.jpg" alt="" />
+        })}
+
+      </span> 
       
     </div>
     </>   
