bbdaa2807cdc81016262618c0e0d6d5070b445df
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2024-07-04 23:05:31 +0530

    ninth commit

--- client/src/pages/PostingPage.jsx
+++ client/src/pages/PostingPage.jsx
@@ -1,14 +1,44 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 const PostingPage = () => {
 
   const [foodData , setFoodData] = useState({
     foodname : '',
-    nature : '',
+    nature : 'Vegetarian',
     country : '',
     recipe : '',
     imageURL : ''
   })
+
+  const handleFoodnameChange = (event) => {
+    setFoodData({...foodData , foodname : event.target.value})
+  }
+
+  const handleFoodNatureChange = (event) => {
+    setFoodData({...foodData , nature : event.target.value})
+  }
+
+  const handleFoodCountryChange = (event) => {
+    setFoodData({...foodData , country : event.target.value})
+  }
+
+  const handleFoodRecipeChange = (event) => {
+    setFoodData({...foodData , recipe : event.target.value})
+  }
+
+  const handleFoodIMGChange = (event) => {
+    setFoodData({...foodData , imageURL : event.target.value})
+  }
+
+  const handleSubmitFood = async () => {
+    console.log('clicked' , foodData)
+    
+    await axios.post('http://localhost:8000/food' , 
+      {...foodData}, 
+    )
+  }
+
 
   return (
     <div className='postingpage_wrapper w-screen h-screen flex flex-col items-center'>
@@ -17,52 +47,26 @@ const PostingPage = () => {
       <form className='w-72 h-3/5 mt-4 flex flex-col text-center justify-around chakra-petch-bold'>
 
         <label className=' bg-gray-600 rounded-md ' htmlFor="foodname">FoodName*</label>
-        <input className='w-full bg-white text-black rounded-md' type="text" name="foodname" id="foodname" onChange={(e => {
-          {setFoodData(
-            foodData.foodname = e.target.value
-          )}
-        })}/>
+        <input className='w-full bg-white text-black rounded-md' type="text" name="foodname" id="foodname" value={foodData.foodname} onChange={handleFoodnameChange} />
 
         <label className=' bg-gray-600 rounded-md ' htmlFor="nature">Nature*</label>
-        <select className='w-full bg-white text-black rounded-md text-center' name="" id="nature" onChange={(e => {
-          {setFoodData(
-            ...foodData,
-            nature = e.target.value
-          )}
-        })}>
+        <select className='w-full bg-white text-black rounded-md text-center' name="" id="nature" value={foodData.nature} onChange={handleFoodNatureChange}>
           <option value="Vegetatrian">Vegetatrian</option>
           <option value="Non-Vegetarian">Non-Vegetarian</option>
         </select>
 
         <label className=' bg-gray-600 rounded-md ' htmlFor="country">Country</label>
-        <input className='w-full bg-white text-black rounded-md' type="text" name="country" id="country" onChange={(e => {
-          {setFoodData(
-            ...foodData,
-            country = e.target.value
-          )}
-        })}/>
+        <input className='w-full bg-white text-black rounded-md' type="text" name="country" id="country" value={foodData.country} onChange={handleFoodCountryChange}/>
 
         <label className=' bg-gray-600 rounded-md ' htmlFor="recipe">Recipe*</label>
-        {/* <input className='w-full bg-white text-black rounded-md' type="text" name="recipe" id="recipe" /> */}
-        <textarea className='w-full h-auto bg-white text-black rounded-md' name="" id="recipe" cols="30" rows="4" onChange={(e => {
-          {setFoodData(
-            ...foodData,
-            recipe = e.target.value
-          )}
-        })}>
-        </textarea>
+        <textarea className='w-full h-auto bg-white text-black rounded-md' name="" id="recipe" cols="30" rows="4" value={foodData.recipe} onChange={handleFoodRecipeChange}></textarea>
         
         <label className=' bg-gray-600 rounded-md ' htmlFor="imageURL">imageURL</label>
-        <input className='w-full bg-white text-black rounded-md' type="text" name="imageURL" id="imageURL" onChange={(e => {
-          {setFoodData(
-            ...foodData,
-            imageURL = e.target.value
-          )}
-        })}/>
+        <input className='w-full bg-white text-black rounded-md' type="text" name="imageURL" id="imageURL" value={foodData.imageURL} onChange={handleFoodIMGChange}/>
         
       </form>
 
-      <button className='chakra-petch-light w-24 h-8 m-4 border bg-white text-[#242424]' type='submit' onClick={console.log(foodData)}>Post</button>
+      <button className='chakra-petch-light w-24 h-8 m-4 border bg-white text-[#242424]' type='submit' onClick={handleSubmitFood}>Post</button>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import cors from 'cors'
+
 import connectMongoDB from './connection.js'
 import userRouter from './routes/user.js'
 import foodRouter from './routes/food.js'
@@ -6,8 +8,10 @@ import foodRouter from './routes/food.js'
 const PORT = 8000 
 
 const app = express()
+app.use(cors())
 
 app.use(express.urlencoded({extended : false}))
+app.use(express.json());
 
 connectMongoDB('mongodb+srv://Onkar:Xiangling@recipeappdb.dfa7tes.mongodb.net/?retryWrites=true&w=majority&appName=RecipeAppDB')
 
